9f137667e78d46f1e442e717a123ef716a05e0fd
shravanmukkera <118331600+shravanmukkera@users.noreply.github.com>
2023-01-25 15:13:46 +0530

    Add files via upload
    printing 0 where 1 present in a 2d array of 5*6 matrix

---
+++ JLQ7_TwoD_Array_0000.java
@@ -0,0 +1,41 @@
+package logical_sat;
+
+public class JLQ7_TwoD_Array_0000 {
+    public static void main(String[] args) {
+        char x[][] = {
+                {'a', 'b', 'c', 'd', 'e', 'f'},
+                {'3', 't', '1', 'k', 'r', 's'},
+                {'a', 'd', 'i', 'l', 'q', 't'},
+                {'b', 'e', 'h', 'm', 'p', 'a'},
+                {'c', 'f', 'g', 'n', 'r', 'b'},
+        };
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                if (x[i][j] == '1')
+                {
+                    for (int k = i, l = 0; l <= 5; l++) {
+                        if (l != 2) {
+                           x[k][l]='0';
+                        }
+                        for (int m = 0, n = j; m <= 4; m++) {
+                            if (m != 1) {
+                                x[m][n] = '0';
+                            }
+                        }
+                    }
+                }
+            }//inner for loop close
+        }//outer for loop close
+        System.out.println("---------------");
+        for (int i=0;i<5;i++)
+        {
+            for (int j=0;j<6;j++)
+            {
+                System.out.print(x[i][j]+"  ");
+            }
+            System.out.println();
+        }
+    }
+}
